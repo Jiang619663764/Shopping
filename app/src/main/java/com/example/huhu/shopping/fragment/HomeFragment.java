9ec35d675be8a6624d2c57618c8a5e43af4c5f5d
jpmph <619663764@qq.com>
2016-05-13 17:51:44 +0800

    add banner

--- app/src/main/java/com/example/huhu/shopping/fragment/HomeFragment.java
+++ app/src/main/java/com/example/huhu/shopping/fragment/HomeFragment.java
@@ -14,6 +14,9 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.example.huhu.shopping.DetailActivity;
 import com.example.huhu.shopping.R;
 import com.example.huhu.shopping.adapter.ProductAdapter;
@@ -31,6 +34,8 @@ import cn.bmob.v3.listener.FindListener;
  */
 public class HomeFragment extends android.support.v4.app.Fragment {
 
+    private SliderLayout mSliderLayout;
+
     private PullToRefreshListView mRefreshListView;
 
     public static List<ProductInfo> mDatas;
@@ -46,37 +51,66 @@ public class HomeFragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_home, container, false);
         getInfo(getActivity());
+        View view=inflater.inflate(R.layout.fragment_home, container, false);
+
+        View layout= LayoutInflater.from(getActivity()).inflate(R.layout.layout_banner, null);
+
         mRefreshListView= (PullToRefreshListView) view.findViewById(R.id.refresh_home_frg);
+        ListView listView = mRefreshListView.getRefreshableView();
+        //轮播图的设定
+        mSliderLayout= (SliderLayout) layout.findViewById(R.id.slider);
+        addBanner();
+        mSliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mSliderLayout.setCustomAnimation(new DescriptionAnimation());
+        mSliderLayout.setPresetTransformer(SliderLayout.Transformer.RotateUp);
+        mSliderLayout.setDuration(3000);
+        listView.addHeaderView(layout);
+
+
         mHandler=new Handler(){
             @Override
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
                 mDatas= (List<ProductInfo>) msg.obj;
                 mProductAdapter=new ProductAdapter(getActivity(),mDatas);
-
                 //设置适配器
                 mRefreshListView.setAdapter(mProductAdapter);
-                //子选项点击事件
-                mRefreshListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Intent intent=new Intent(getActivity(), DetailActivity.class);
-                        intent.putExtra("position",position-1);
-                        startActivity(intent);
-                    }
-                });
-                //刷新数据
-                mRefreshListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
-                    @Override
-                    public void onRefresh(PullToRefreshBase<ListView> refreshView) {
 
-                    }
-                });
             }
         };
+        //子选项点击事件
+        mRefreshListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent(getActivity(), DetailActivity.class);
+                intent.putExtra("position",position-1);
+                startActivity(intent);
+            }
+        });
+        //刷新数据
+        mRefreshListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
+            @Override
+            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+
+            }
+        });
         return view;
+    }
+
+    private void addBanner(){
+        TextSliderView tsv=new TextSliderView(getActivity());
+        tsv.image(R.mipmap.banner1);
+        tsv.description("一分钱就能抢");
+        mSliderLayout.addSlider(tsv);
+        TextSliderView tsv1=new TextSliderView(getActivity());
+        tsv1.image(R.mipmap.banner2);
+        tsv1.description("吃货的季节");
+        mSliderLayout.addSlider(tsv1);
+        TextSliderView tsv2=new TextSliderView(getActivity());
+        tsv2.image(R.mipmap.banner3);
+        tsv2.description("好吃来不停");
+        mSliderLayout.addSlider(tsv2);
     }
 
     /**
